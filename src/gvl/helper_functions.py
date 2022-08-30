d4575a5ceb763e9099df5f65184d49dfdb75bf5e
chrise96 <chrise96@hotmail.com>
2022-08-30 09:46:18 +0200

    Delete files

--- src/gvl/helper_functions.py
+++ src/gvl/helper_functions.py
@@ -15,14 +15,6 @@ from upcp.utils import math_utils
 DEFAULT_BOX_SIZE = 1000
 
 
-def roundup(x, N=DEFAULT_BOX_SIZE):
-    return x - x % -N
-
-
-def rounddown(x, N=DEFAULT_BOX_SIZE):
-    return x - x % +N
-
-
 def box_to_name(box, box_size):
     (x_min, y_min, _, _) = box.bounds
     return f'{x_min/box_size:.0f}_{y_min/box_size:.0f}'
@@ -259,39 +251,3 @@ def former_preprocess_now_add_pid(points):
     f_pts = pd.DataFrame(points)
     f_pts['pid'] = f_pts.index
     return f_pts
-
-
-def color_clusters(grouped_points):
-    colors = get_colors(len(np.unique(grouped_points['Classification'])))
-    output_dataframe = grouped_points[['pid',
-                                       'X', 'Y', 'Z']]
-    for i, color in enumerate(['Red', 'Green', 'Blue']):
-        col = output_dataframe.apply(
-                lambda row: colors[int(row['Classification'])][i], axis=1)
-        output_dataframe.loc[:, color] = col
-
-    return output_dataframe
-
-
-def get_colors(n):
-    cols = 100 * [[0, 0, 0], [1, 0, 103], [213, 255, 0], [255, 0, 86],
-                  [158, 0, 142], [14, 76, 161], [255, 229, 2],
-                  [0, 95, 57], [0, 255, 0], [149, 0, 58], [255, 147, 126],
-                  [164, 36, 0], [0, 21, 68], [145, 208, 203], [98, 14, 0],
-                  [107, 104, 130], [0, 0, 255], [0, 125, 181],
-                  [106, 130, 108], [0, 174, 126], [194, 140, 159],
-                  [190, 153, 112], [0, 143, 156], [95, 173, 78], [255, 0, 0],
-                  [255, 0, 246], [255, 2, 157], [104, 61, 59],
-                  [255, 116, 163], [150, 138, 232], [152, 255, 82],
-                  [167, 87, 64], [1, 255, 254], [255, 238, 232],
-                  [254, 137, 0], [189, 198, 255], [1, 208, 255], [187, 136, 0],
-                  [117, 68, 177], [165, 255, 210], [255, 166, 254],
-                  [119, 77, 0], [122, 71, 130], [38, 52, 0], [0, 71, 84],
-                  [67, 0, 44], [181, 0, 255], [255, 177, 103],
-                  [255, 219, 102], [144, 251, 146], [126, 45, 210],
-                  [189, 211, 147], [229, 111, 254], [222, 255, 116],
-                  [0, 255, 120], [0, 155, 255], [0, 100, 1], [0, 118, 255],
-                  [133, 169, 0], [0, 185, 23], [120, 130, 49], [0, 255, 198],
-                  [255, 110, 65], [232, 94, 190]]
-
-    return sample(cols, n)
